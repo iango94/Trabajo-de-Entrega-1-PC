4eb1583b2d610213a9df287c5840857af67f249e
ian <iango_93@hotmail.com>
2026-04-09 21:01:09 -0500

    Ian Gomez - Busqueda

--- main.py
+++ main.py
@@ -9,10 +9,37 @@ def cargar_datos(nombre_archivo):
   
 mis_datos = cargar_datos("diabetes_50FILAS.csv")
 print(f"Se cargaron {len(mis_datos)} registros con éxito.")
+print(mis_datos[0])
+
+def buscar():
+    busqueda = []
+    a = input("digite criterio busqueda: ")
+    b = input("digite 1 para busqueda exacta y 2 para busqueda extendida: ")
+    
+    if b == "1": 
+        print("jjhjh")
+        for linea in mis_datos:
+            for celda in linea:
+                if float(a) == float(celda):
+                    busqueda.append(linea)
+                    break
+    else:
+        for linea2 in mis_datos:
+            r = " ".join(linea2)
+            x = r
+            if a in x:
+                busqueda.append(linea2)
+
+    print(f"Se encontraron {len(busqueda)} resultados")
+
+    for linea in busqueda:
+        print(linea)
+
+
 
 def menu_interactivo():
     while True:
-        print("\n"¡BIENVENIDOS AL MENÚ INTERACTIVO DE INSULINE_LOGIC!"")
+        print("\n BIENVENIDOS AL MENÚ INTERACTIVO DE INSULINE_LOGIC!")
         print("Ingresa 1 si deseas BUSCAR")
         print("Ingresa 2 si deseas realizar ESTADÍSTICAS BÁSICAS")
         print("Ingresa 3 si deseas FILTRAR POR CONDICIÓN")
@@ -22,6 +49,7 @@ def menu_interactivo():
         match opcion:
             case 1:
                 print("Elegiste BUSCAR")
+                buscar()
             case 2:
                 print("Elegiste ESTADÍSTICAS BÁSICAS")
             case 3:
@@ -31,5 +59,7 @@ def menu_interactivo():
                 break
             case _:
                 print("Opción no válida. Intenta nuevamente.")
-menu()   
-              
+
+
+menu_interactivo()
+
